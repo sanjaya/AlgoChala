12dc0bc046325a557f2e0e43f7ba5e61a0731d26
sanjayapanigrahy <sanjaya.panigrahy@gmail.com>
2025-03-27 22:03:50 +0530

    Q4 solved

---
+++ arrays/Reversearray.py
@@ -0,0 +1,11 @@
+def reverseArray(lst):
+    start = 0
+    end = len(lst)-1
+    while start < end:
+        lst[start], lst[end] = lst[end], lst[start]
+        start += 1
+        end -= 1
+    return lst
+
+
+print(reverseArray([1, 2, 3, 4, 5]))
