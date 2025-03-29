a43eca119421b7f0d3f62c5fa1c1b8c1f631aee4
sanjayapanigrahy <sanjaya.panigrahy@gmail.com>
2025-03-29 17:12:03 +0530

    file name chnage

--- arrays/two_number_sum.py
+++ arrays/two_number_sum.py
@@ -14,7 +14,23 @@ class TwoNumberSum:
                 right -= 1
         return []
 
+    def twoNumberSum(array, targetSum):
+        complements = {}
+        for num in array:
+            complement = targetSum - num
+            if complement in complements:
+                return [num,complement]
+            else:
+                complements[num] = "Available"
+        return[]
+
 # Call the method directly since it's static
 output = TwoNumberSum.twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)
 print(output)
 # Time O(nlon(n)) | sapce O(1) - More stable compare to hash based solution as it takes O(n) sapce.
+
+
+# Call the method directly since it's static
+output = TwoNumberSum.twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 16)
+print(output)
+#O(n) time and  O(n) space. Not considered to be stable.
